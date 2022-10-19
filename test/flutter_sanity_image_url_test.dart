7cced75ec6c115360fe09c57ac45006e517b7287
Mark Bruderer <markbruderer123@gmail.com>
2022-10-19 18:13:46 +0200

    clean up repo.

--- test/flutter_sanity_image_url_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_sanity_image_url/flutter_sanity_image_url.dart';
-
-void main() {
-  test('adds one to input values', () {});
-}
