67d29c8ce15bfc368385bcf9fda50d9c54f05fb5
Mark Bruderer <markbruderer123@gmail.com>
2023-12-09 16:15:44 +0100

    feat: export placeholder widget.

--- lib/flutter_sanity_image_url.dart
+++ lib/flutter_sanity_image_url.dart
@@ -2,3 +2,4 @@ export 'src/builder.dart';
 export 'src/model/sanity_image.dart';
 export 'src/model/sanity_palette.dart';
 export 'src/model/sanity_asset.dart';
+export 'src/image_placeholder.dart';
